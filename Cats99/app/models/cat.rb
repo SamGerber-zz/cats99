e1ab26e3467667116827a465920d4cfdc7597549
SamGerber <gerbyxvi@gmail.com>
2016-01-26 14:53:11 -0800

    Create Cat model

--- Cats99/app/models/cat.rb
+++ Cats99/app/models/cat.rb
@@ -1,2 +1,13 @@
 class Cat < ActiveRecord::Base
+  COLORS = ['white', 'black', 'red', 'blue', 'cream', 'brown', 'cinnamon']
+  SEX = ['M', 'F']
+
+  validates :name, :birth_date, :sex, :color, presence: true
+  validates :color, inclusion: COLORS
+  validates :sex, inclusion: SEX
+
+  def age
+    (Date.today - self.birth_date) / 365
+  end
+
 end
