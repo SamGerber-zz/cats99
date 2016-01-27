ff132e4ea9107cb8ac4033cdd2f570c4677f53e2
SamGerber <gerbyxvi@gmail.com>
2016-01-27 07:01:06 -0800

    Implement #new and #create for CatRentalRequestsController

---
+++ Cats99/app/controllers/cat_rental_requests_controller.rb
@@ -0,0 +1,28 @@
+class CatRentalRequestsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @cat_rental_request = CatRentalRequest.new(cat_rental_requests_params)
+    if @cat_rental_request.save
+      redirect_to cat_url(@cat_rental_request.cat_id)
+    else
+      render(
+        json: @cat_rental_request.errors.full_messages,
+        status: :unprocessable_entity
+      )
+    end
+  end
+
+
+  private
+
+  def cat_rental_requests_params
+    params.require(:cat_rental_request).permit(
+      :cat_id,
+      :start_date,
+      :end_date
+    )
+  end
+end
