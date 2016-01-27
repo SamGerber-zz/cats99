13f0d1c7c84f306ff8f678bd1412f36673e1a257
SamGerber <gerbyxvi@gmail.com>
2016-01-27 06:56:00 -0800

    Add relation #cat_rental_requests

--- Cats99/app/models/cat.rb
+++ Cats99/app/models/cat.rb
@@ -6,6 +6,9 @@ class Cat < ActiveRecord::Base
   validates :color, inclusion: COLORS
   validates :sex, inclusion: SEX
 
+  has_many :cat_rental_requests,
+    dependent: :destroy
+
   def age
     (Date.today - self.birth_date) / 365
   end
